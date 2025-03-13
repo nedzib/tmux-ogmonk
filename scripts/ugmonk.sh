cefa78926c468546188a07858c125fbfa1154b25
Nedzib Sastoque <nedzib.sastoque@gmail.com>
2025-03-13 07:49:25 -0500

    refactor: formatting

--- scripts/ugmonk.sh
+++ scripts/ugmonk.sh
@@ -5,84 +5,81 @@ TODAY=$(date "+%Y-%m-%d")
 
 # Create the database and table if they don't exist
 initialize_db() {
-    sqlite3 "$DB_FILE" <<EOF
-CREATE TABLE IF NOT EXISTS tasks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    status INTEGER NOT NULL DEFAULT 0,
-    task TEXT NOT NULL,
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+  sqlite3 "$DB_FILE" <<EOF
+  CREATE TABLE IF NOT EXISTS tasks (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  status INTEGER NOT NULL DEFAULT 0,
+  task TEXT NOT NULL,
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+  );
 EOF
 }
 
-
 show_tasks() {
-    local tasks=()
-    local line_number=0
-    local date=$1
+  local tasks=()
+  local line_number=0
+  local date=$1
 
-    while IFS='|' read -r id status task; do
-        line_number=$((line_number + 1))
-        case "$status" in
-            '0') status_text="󰄰" ;;
-            '1') status_text="󱎖" ;;
-            '2') status_text="󰄯" ;;
-            '3') status_text="󰪌" ;;
-            '4') status_text="󱖘" ;;
-        esac
-        tasks+=("${status_text} ${task}" "${line_number}" "run-shell '$0 toggle ${id} $date'")
-    done < <(tasks_by_date "$date")
+  while IFS='|' read -r id status task; do
+    line_number=$((line_number + 1))
+    case "$status" in
+      '0') status_text="󰄰" ;;
+      '1') status_text="󱎖" ;;
+      '2') status_text="󰄯" ;;
+      '3') status_text="󰪌" ;;
+      '4') status_text="󱖘" ;;
+    esac
+    tasks+=("${status_text} ${task}" "${line_number}" "run-shell '$0 toggle ${id} $date'")
+  done < <(tasks_by_date "$date")
 
-    prev_date=$(date -d "$date - 1 day" "+%Y-%m-%d")
-    next_date=$(date -d "$date + 1 day" "+%Y-%m-%d")
+  prev_date=$(date -d "$date - 1 day" "+%Y-%m-%d")
+  next_date=$(date -d "$date + 1 day" "+%Y-%m-%d")
 
+  menu=(
+    "$(formatted_date "$date")"
+  )
 
-    menu=(
-      "$(formatted_date "$date")"
-    )
+  if [[ ${#tasks[@]} -eq 0 ]]; then
+    menu+=("There is no task for this day" "" "")
+  else
+    menu+=("${tasks[@]}")
+  fi
 
-    if [[ ${#tasks[@]} -eq 0 ]]; then
-      menu+=("There is no task for this day" "" "")
-    else
-      menu+=("${tasks[@]}")
-    fi
+  menu+=(
+  ""
+  )
 
+  if [[ "$date" == "$TODAY" ]]; then
     menu+=(
-    ""
-    )
+    "New task" "n" "run-shell '$0 add'"
+  )
+  fi
 
-    if [[ "$date" == "$TODAY" ]]; then
-      menu+=(
-      "New task" "n" "run-shell '$0 add'"
-    )
-    fi
+  menu+=(
+  "Delete" "d" "run-shell '$0 remove $date'"
+  "<< Previous" "h" "run-shell '$0 show $prev_date'"
+  )
 
-    menu+=(
-    "Delete" "d" "run-shell '$0 remove $date'"
-    "<< Previous" "h" "run-shell '$0 show $prev_date'"
-    )
+  if [[ "$date" != "$TODAY" ]]; then
+    menu+=("Next >>" "l" "run-shell '$0 show $next_date'")
+  fi
 
-    if [[ "$date" != "$TODAY" ]]; then
-      menu+=("Next >>" "l" "run-shell '$0 show $next_date'")
-    fi
+  menu+=(
+  ""
+  "Help" "?" "run-shell '$0 help'"
+  )
 
-    menu+=(
-    ""
-    "Help" "?" "run-shell '$0 help'"
-    )
-
-
-    tmux display-menu -x C -y C -T "${menu[@]}"
+  tmux display-menu -x C -y C -T "${menu[@]}"
 }
 
 
 formatted_date() {
-    date -d "$1" "+%Y-%m-%d"
+  date -d "$1" "+%Y-%m-%d"
 }
 
 tasks_by_date() {
-    local date="$1"
-    sqlite3 "$DB_FILE" "SELECT id, status, task FROM tasks WHERE DATE(created_at) = '$date' ORDER BY created_at"
+  local date="$1"
+  sqlite3 "$DB_FILE" "SELECT id, status, task FROM tasks WHERE DATE(created_at) = '$date' ORDER BY created_at"
 }
 
 task_count_by_date() {
@@ -91,24 +88,24 @@ task_count_by_date() {
 }
 
 add_task() {
-    local task_count
-    task_count=$(task_count_by_date "$TODAY")
+  local task_count
+  task_count=$(task_count_by_date "$TODAY")
 
-    if [ "$task_count" -ge 9 ]; then
-        tmux display-message "You cannot add more than 9 tasks per day."
-        $0 show
-        return
-    fi
+  if [ "$task_count" -ge 9 ]; then
+    tmux display-message "You cannot add more than 9 tasks per day."
+    $0 show
+    return
+  fi
 
-    tmux command-prompt -p "New task: " "run-shell 'sqlite3 $DB_FILE \"INSERT INTO tasks (status, task) VALUES (1, \\\"%1\\\")\"; $0 show'"
+  tmux command-prompt -p "New task: " "run-shell 'sqlite3 $DB_FILE \"INSERT INTO tasks (status, task) VALUES (1, \\\"%1\\\")\"; $0 show'"
 }
 
 insert_task() {
-    sqlite3 "$DB_FILE" "INSERT INTO tasks (task) VALUES ('$1')"
+  sqlite3 "$DB_FILE" "INSERT INTO tasks (task) VALUES ('$1')"
 }
 
 remove_task() {
-    tmux command-prompt -p "Task number to delete: " "run-shell '$0 delete %1 $1'"
+  tmux command-prompt -p "Task number to delete: " "run-shell '$0 delete %1 $1'"
 }
 
 delete_task() {
@@ -136,66 +133,65 @@ delete_task() {
 }
 
 change_status() {
-    current_status=$(sqlite3 "$DB_FILE" "SELECT status FROM tasks WHERE id = $1")
+  current_status=$(sqlite3 "$DB_FILE" "SELECT status FROM tasks WHERE id = $1")
 
-    if [ "$current_status" -eq 4 ]; then
-        new_status=0
-    else
-        new_status=$((current_status + 1))
-    fi
+  if [ "$current_status" -eq 4 ]; then
+    new_status=0
+  else
+    new_status=$((current_status + 1))
+  fi
 
-    sqlite3 "$DB_FILE" "UPDATE tasks SET status = $new_status WHERE id = $1"
-    $0 show $2
+  sqlite3 "$DB_FILE" "UPDATE tasks SET status = $new_status WHERE id = $1"
+  $0 show $2
 }
 
 help_ogmonk() {
-    tmux display-popup -w 50% -h 50% "echo 'Symbols and their meanings:
-󰄰 - Pending
-󱎖 - In Progress
-󰄯 - Done
-󰪌 - Meeting
-󱖘 - Delegated
+  tmux display-popup -w 50% -h 50% "echo 'Symbols and their meanings:
+  󰄰 - Pending
+  󱎖 - In Progress
+  󰄯 - Done
+  󰪌 - Meeting
+  󱖘 - Delegated
 
-press esc'"
-    $0 show
+  press esc'"
+  $0 show
 }
 
 popup_menu() {
-    show_tasks "$TODAY"
+  show_tasks "$TODAY"
 }
 
 main() {
-    initialize_db
-    cmd="$1"
-    shift
+  initialize_db
+  cmd="$1"
+  shift
 
-    case "$cmd" in
-        show)
-            show_tasks "$@"
-            ;;
-        add)
-            add_task
-            ;;
-        remove)
-            remove_task "$@"
-            ;;
-        toggle)
-            change_status "$@"
-            ;;
-        delete)
-            delete_task "$@"
-            ;;
-        menu)
-            popup_menu
-            ;;
-        help)
-            help_ogmonk
-            ;;
-        *)
-            echo "Usage: $0 {show|add|remove|toggle|delete|menu|help}"
-            ;;
-    esac
+  case "$cmd" in
+    show)
+      show_tasks "$@"
+      ;;
+    add)
+      add_task
+      ;;
+    remove)
+      remove_task "$@"
+      ;;
+    toggle)
+      change_status "$@"
+      ;;
+    delete)
+      delete_task "$@"
+      ;;
+    menu)
+      popup_menu
+      ;;
+    help)
+      help_ogmonk
+      ;;
+    *)
+      echo "Usage: $0 {show|add|remove|toggle|delete|menu|help}"
+      ;;
+  esac
 }
 
 main "$@"
-
